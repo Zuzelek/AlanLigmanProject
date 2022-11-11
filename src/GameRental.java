06206f6475dd05c35968af5f81ba4f431f7af680
t00223033 <alan35691@gmail.com>
2022-11-11 10:32:11 +0000

    Third Commit

--- src/GameRental.java
+++ src/GameRental.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
+import javax.swing.border.Border;
 
 
 public class GameRental extends JFrame implements ActionListener {
@@ -11,60 +12,84 @@ public class GameRental extends JFrame implements ActionListener {
     private JButton removeGameButton;
     private JButton viewGameButton;
     private JPanel gamePanel;
-
-    private JLabel background;
+    private JLabel logoImage;
+    private JMenu gameOptionMenu;
     public GameRental() {
-        systemFrame = new JFrame("Game System");
+
+            systemFrame = new JFrame("Game System");
+
+
 
         gamePanel = new JPanel();
-
-        /*background = new JLabel();
-        background.setSize(200,200);
-        setLayout(null);
-        ImageIcon img = new ImageIcon("GameRental.PNG");
-        background = new JLabel("",img,JLabel.CENTER);
-        background.setBounds(0,0,200,200);
-        add(background); */
-
+        gamePanel.setLayout(null);
+        gamePanel.setBackground(Color.DARK_GRAY);
         //ADD GAME BUTTON
         addGameButton = new JButton("Add Game");
         addGameButton.setMnemonic('A');
-        addGameButton.setBackground(Color.GREEN);
-        addGameButton.setMargin(new Insets(5,25,5,25));
+        addGameButton.setBackground(Color.LIGHT_GRAY);
+        addGameButton.setMargin(new Insets(15,25,15,25));
+        addGameButton.setFont(new Font("Arial",Font.BOLD,13));
+        addGameButton.setSize(125,50);
+        addGameButton.setLocation(60,150);
+        addGameButton.setFocusPainted(false);
+        addGameButton.setBorder(BorderFactory.createEmptyBorder(5,2,5,2)); // Especially important
+
+
+        addGameButton.setLocation(25,50);
         gamePanel.add(addGameButton);
 
         //REMOVE GAME BUTTON
         removeGameButton = new JButton("Remove Game");
         removeGameButton.setMnemonic(KeyEvent.VK_R);
-        removeGameButton.setBackground(Color.RED);
-        removeGameButton.setMargin(new Insets(5,25,5,25));
+        removeGameButton.setBackground(Color.LIGHT_GRAY);
+        removeGameButton.setForeground(new Color(51,153,255));
+        removeGameButton.setFont(new Font("Arial",Font.BOLD,13));
+        removeGameButton.setSize(135,50);
+        removeGameButton.setLocation(175,50);
         gamePanel.add(removeGameButton);
 
         //VIEW GAME BUTTON
         viewGameButton = new JButton("View Games");
         viewGameButton.setMnemonic(KeyEvent.VK_V);
-        viewGameButton.setBackground(Color.GREEN);
-        viewGameButton.setMargin(new Insets(5,25,5,25));
+        viewGameButton.setBackground(Color.LIGHT_GRAY);
+        viewGameButton.setFont(new Font("Arial",Font.BOLD,13));
+        viewGameButton.setSize(125,50);
+        viewGameButton.setLocation(335,50);
+
         gamePanel.add(viewGameButton);
 
+        logoImage = new JLabel();
+        logoImage.setIcon(new ImageIcon("C:\\Users\\t00223033\\IdeaProjects\\AlanLigmanProject\\src\\GameRentalLogo-removebg-preview.png"));
+        logoImage.setSize(350,450);
+        logoImage.setLocation(60,75);
+        //logoImage.setBounds(50,150);
+        gamePanel.add(logoImage);
 
 
-
-        systemFrame.setSize(500, 500);
+        systemFrame.setSize(500, 700);
         systemFrame.setLocationRelativeTo(null);
         systemFrame.setResizable(false);
 
         systemFrame.add(gamePanel);
         //setIconImage(new ImageIcon(getClass().getResource("bf2042.jpg")).getImage());
 
+        systemFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
         systemFrame.setVisible(true);
     }
     public static void main(String[] args) {
+
         new GameRental();
+    }
+    public void addGame(){
+
     }
 
 
     public void actionPerformed(ActionEvent e){
+        String menuOption = e.getActionCommand();
+
+        if(menuOption == "Add Game" || e.getSource() == this.addGameButton)
+            addGame();
 
     }
 }
--- src/Transaction.java
+++ src/Transaction.java
@@ -1,4 +1,4 @@
-public class Transaction {
+/*public class Transaction {
     private Employee employee;
     private
-}
+}*/
